8dcaef5f7a8091ff5d03e6d4a2fe0521ba397f2d
171250025 <171250025@smail.nju.edu.cn>
2020-06-13 22:57:26 +0800

    update

--- src/main/java/com/example/iot/service/ConnectionService/connection/MQTT/PubClient.java
+++ src/main/java/com/example/iot/service/ConnectionService/connection/MQTT/PubClient.java
@@ -3,6 +3,7 @@ package com.example.iot.service.ConnectionService.connection.MQTT;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,19 +11,27 @@ public class PubClient{
     @Autowired
     PubCallback pubCallback;
 
-    private static final String HOST = "tcp://101.37.80.37:1883";
-    private static final String CLIENTID = "pubClient";
-    private static final String USERNAME = "test";
-    private static final String PASSWORD = "test";
+    @Value("${mqtt.username}")
+    private String username;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    @Value("${mqtt.url}")
+    private String url;
+
+    @Value("${mqtt.sender.clientId}")
+    private String clientId;
+
     private MqttClient mqttClient;
 
     public void connect() throws MqttException {
-        mqttClient = new MqttClient(HOST, CLIENTID, new MemoryPersistence());
+        mqttClient = new MqttClient(url, clientId, new MemoryPersistence());
 
         MqttConnectOptions options = new MqttConnectOptions();
         options.setCleanSession(true);
-        options.setUserName(USERNAME);
-        options.setPassword(PASSWORD.toCharArray());
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
         options.setConnectionTimeout(1000);
         options.setKeepAliveInterval(1800);
         options.setAutomaticReconnect(true);
