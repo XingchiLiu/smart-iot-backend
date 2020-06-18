fc5d996304a241f1ee41a021454bd9277f9ae984
171250025 <171250025@smail.nju.edu.cn>
2020-06-18 11:59:24 +0800

    添加一个HTTP server用于模拟

--- src/main/java/com/example/iot/HTTP/SimpleHttpServer.java
+++ src/main/java/com/example/iot/HTTP/SimpleHttpServer.java
@@ -3,6 +3,9 @@ package com.example.iot.HTTP;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,13 +15,27 @@ import java.net.InetSocketAddress;
  * a simple static http server
  * for test use
  */
-public class SimpleHttpServer {
+@Component
+public class SimpleHttpServer implements ApplicationRunner {
 
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpServer server = null;
+                try {
+                    server = HttpServer.create(new InetSocketAddress(8000), 0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                server.createContext("/test", new MyHandler());
+                server.setExecutor(null); // creates a default executor
+                server.start();
+                System.out.println("HTTP Server Ready!");
+            }
+        }).start();
     }
 
     static class MyHandler implements HttpHandler {
